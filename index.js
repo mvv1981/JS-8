718ca902b2f98f26ad18027093accaa135e5d88e
max-barabash <48650035+max-barabash@users.noreply.github.com>
2019-04-19 16:35:33 +0400

    Update functions

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 /**
+ * Барабаш Максим Сергеевич
  * @param {Array} collection
  * @params {Function[]} – Функции для запроса
  * @returns {Array}
  */
 function query(collection) {
-
+    var handlerCollection = Array.prototype.slice.call(arguments, 1);
+    handlerCollection.sort(function sortfunction(a, b) {
+        if (a.order < b.order) {
+            return -1
+        }
+        if (a.order > b.order) {
+            return 1
+        }
+        else {
+            return 0
+        }
+    });
+    var result = collection;
+    for (var i = 0; i < handlerCollection.length; i++) {
+        result = handlerCollection[i].processParam(result);
+    }
+    return result
 }
 
 /**
  * @params {String[]}
  */
 function select() {
-
+    var selectParam = Array.prototype.slice.call(arguments);
+    var selectObj = {
+        order: 2,
+        processParam: function (collection) {
+            for (var i = 0; i < collection.length; i++) {
+                var objParam = Object.keys(collection[i]);
+                for (var k = 0; k < objParam.length; k++) {
+                    if (selectParam.includes(objParam[k])) {
+                        continue
+                    }
+                    else {
+                        delete collection[i][objParam[k]]
+                    }
+                }
+            }            
+            return collection
+        }
+    }
+    return selectObj
 }
 
 /**
@@ -19,7 +54,21 @@ function select() {
  * @param {Array} values – Массив разрешённых значений
  */
 function filterIn(property, values) {
-
+    var filterObj = {
+        order: 1,
+        processParam: function (collection) {
+            var result = collection.filter(function (personal) {
+                if (personal.hasOwnProperty(property)) {
+                    if (values.includes(personal[property])) {
+                        return true;
+                    }
+                }
+                return false;
+            });
+            return result
+        }
+    }
+    return filterObj;
 }
 
 module.exports = {
